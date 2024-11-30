eb7e99ed3e3dc9acf2d3ef4d63fb011aec0fa0e0
OshkarVTec <a01275287@tec.mx>
2024-11-30 08:56:08 -0600

    add education data

---
+++ src/data/education.ts
@@ -0,0 +1,13 @@
+export const educationData = [
+    {
+        school: 'Tec de Monterrey (ITESM) Puebla',
+        name: 'Bachelor of Science in Computer Science and Technology',
+        date: 'August 2020 - Present',
+        score: 'GPA 98/100'
+    },
+    {
+        school: 'Tec de Monterrey (ITESM) Querétaro',
+        name: 'Concentration in advanced Artificial Intelligence for Data Science',
+        date: 'August 2024 - December 2025'
+    }
+];
