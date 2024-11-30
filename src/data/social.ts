71fb1cf16986f446a5aa60a5b0d212bcfd202f0c
OshkarVTec <a01275287@tec.mx>
2024-11-30 09:59:08 -0600

    add contact

---
+++ src/data/social.ts
@@ -0,0 +1,14 @@
+export const socialData = [
+    {
+        platform: 'Github',
+        icon: 'fa6-brands:github',
+        color: '#171515',
+        link: 'https://github.com/OshkarVTec'
+    },
+    {
+        platform: 'LinkedIn',
+        icon: 'fa6-brands:linkedin',
+        color: '#0072b1',
+        link: 'https://www.linkedin.com/in/oskar-adolfo-villa-397000257/'
+    }
+];
