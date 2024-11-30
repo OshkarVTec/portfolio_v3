913766cca59eaa7e8a78a7fb6b9b139ba2f1442d
OshkarVTec <a01275287@tec.mx>
2024-11-30 08:54:22 -0600

    format with prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			keyframes: {
-				slideInLeft: {
-					"0%": { opacity: 1, transform: "translateX(100%)" },
-					"100%": { opacity: 1, transform: "translateX(0)" },
-				},
-				slideInRight: {
-					"0%": { opacity: 1, transform: "translatex(-100%)" },
-					"100%": { opacity: 1, transform: "translateX(0)" },
-				},
-			},
-			animation: {
-				slideInLeft: "slideInLeft .5s ease-in-out forwards",
-				slideInRight: "slideInRight .5s ease-in-out forwards",
-			},
-		},
-		plugins: [],
-	},
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            keyframes: {
+                slideInLeft: {
+                    '0%': { opacity: 1, transform: 'translateX(100%)' },
+                    '100%': { opacity: 1, transform: 'translateX(0)' }
+                },
+                slideInRight: {
+                    '0%': { opacity: 1, transform: 'translatex(-100%)' },
+                    '100%': { opacity: 1, transform: 'translateX(0)' }
+                }
+            },
+            animation: {
+                slideInLeft: 'slideInLeft .5s ease-in-out forwards',
+                slideInRight: 'slideInRight .5s ease-in-out forwards'
+            }
+        },
+        plugins: []
+    }
 };
