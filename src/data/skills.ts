9235c48ab1c2594ad2d430295a35f4835326f330
OshkarVTec <a01275287@tec.mx>
2024-11-30 09:46:34 -0600

    add skills

---
+++ src/data/skills.ts
@@ -0,0 +1,14 @@
+export const skillsData = [
+    { name: 'ReactJS', icon: 'fa6-brands:react', color: '#61D8FB' },
+    { name: 'Python', icon: 'fa6-brands:python', color: '#ffde57' },
+    { name: 'C++', icon: 'tabler:brand-cpp' },
+    { name: 'Embedded systems', icon: 'fa6-solid:microchip' },
+    { name: 'TypeScript', icon: 'fa6-brands:js', color: '#3178c6' },
+    { name: 'JavaScript', icon: 'fa6-brands:js', color: '#F0DB4F' },
+    { name: 'HTML5', icon: 'fa6-brands:html5', color: '#e35c26' },
+    { name: 'CSS3', icon: 'fa6-brands:css3-alt', color: '#264de4' },
+    { name: 'Git', icon: 'fa6-brands:git-alt', color: '#F1502F' },
+    { name: 'Matlab', icon: 'fa6-solid:chart-line', color: '#49c3c3' },
+    { name: 'MySQL', icon: 'fa6-solid:database', color: '#00758F' },
+    { name: 'Node.js', icon: 'fa6-brands:node-js', color: '#3c873a' }
+];
